af3d1dcc26084644a57cc1ca07143ca09280a95f
Harro Verton <wanwizard@openpli.org>
2018-02-22 13:39:24 +0100

    add search on 'title ends with'

--- autotimer/src/AutoTimerComponent.py
+++ autotimer/src/AutoTimerComponent.py
@@ -153,7 +153,7 @@ class AutoTimerComponent(object):
 	searchCase = property(lambda self: self._searchCase, setSearchCase)
 
 	def setSearchType(self, type):
-		assert type in ("exact", "partial", "start", "description"), "search type must be exact, partial, start or description"
+		assert type in ("exact", "partial", "start", "end", "description"), "search type must be exact, partial. start, end start or description"
 		self._searchType = type
 
 	searchType = property(lambda self: self._searchType, setSearchType)
